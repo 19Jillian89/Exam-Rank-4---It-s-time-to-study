b8b51daf8c639fbe0329cf35f99437ccde542411
Ilaria Nassi <110412503+19Jillian89@users.noreply.github.com>
2026-06-09 19:23:15 +0200

    Refactor picoshell and related functions for clarity

--- Level 1/Picoshell/picoshell.c
+++ Level 1/Picoshell/picoshell.c
@@ -5,14 +5,10 @@
 /*
 ** execute_child:
 ** Configura stdin/stdout del child ed esegue il comando.
-**
-** - prev: fd di input dal comando precedente (-1 se non esiste)
-** - fd[2]: pipe verso il prossimo comando
-** - has_next_cmd: indica se esiste un comando successivo
 */
 void	execute_child(char **cmd, int prev, int fd[2], int has_next_cmd)
 {
-	/* stdin <- pipe precedente */
+	/* 1. Gestione INPUT (dal comando precedente) */
 	if (prev != -1)
 	{
 		if (dup2(prev, STDIN_FILENO) == -1)
@@ -20,12 +16,11 @@ void	execute_child(char **cmd, int prev, int fd[2], int has_next_cmd)
 		close(prev);
 	}
 
-	/* stdout -> pipe successiva */
+	/* 2. Gestione OUTPUT (verso il prossimo comando) */
 	if (has_next_cmd)
 	{
 		if (dup2(fd[1], STDOUT_FILENO) == -1)
 			exit(1);
-
 		close(fd[0]);
 		close(fd[1]);
 	}
@@ -37,18 +32,12 @@ void	execute_child(char **cmd, int prev, int fd[2], int has_next_cmd)
 /*
 ** fork_execute:
 ** Crea il child ed aggiorna i fd nel parent.
-**
-** Return:
-**   0 -> success
-**   1 -> error
 */
-int	fork_execute(char **cmd, int *prev,
-			int fd[2], int has_next_cmd)
+int	fork_execute(char **cmd, int *prev, int fd[2], int has_next_cmd)
 {
 	pid_t	pid;
 
 	pid = fork();
-
 	if (pid == -1)
 	{
 		if (has_next_cmd)
@@ -56,6 +45,8 @@ int	fork_execute(char **cmd, int *prev,
 			close(fd[0]);
 			close(fd[1]);
 		}
+		if (*prev != -1)
+			close(*prev);
 		return (1);
 	}
 
@@ -63,7 +54,6 @@ int	fork_execute(char **cmd, int *prev,
 		execute_child(cmd, *prev, fd, has_next_cmd);
 
 	/* parent */
-
 	if (*prev != -1)
 		close(*prev);
 
@@ -79,13 +69,9 @@ int	fork_execute(char **cmd, int *prev,
 }
 
 /*
-** picoshell:
 ** Esegue una pipeline di comandi.
-**
-** Esempio:
-**   ls | grep .c | wc -l
 */
-int	picoshell(char ***cmds)
+int	picoshell(char **cmds[])
 {
 	int	fd[2];
 	int	prev;
@@ -96,10 +82,8 @@ int	picoshell(char ***cmds)
 
 	prev = -1;
 	i = 0;
-
 	while (cmds[i])
 	{
-		/* crea pipe solo se esiste un comando successivo */
 		if (cmds[i + 1] != NULL)
 		{
 			if (pipe(fd) == -1)
@@ -110,8 +94,7 @@ int	picoshell(char ***cmds)
 			}
 		}
 
-		if (fork_execute(cmds[i], &prev,
-				fd, cmds[i + 1] != NULL))
+		if (fork_execute(cmds[i], &prev, fd, cmds[i + 1] != NULL))
 			return (1);
 
 		i++;
@@ -119,9 +102,6 @@ int	picoshell(char ***cmds)
 
 	while (wait(NULL) > 0)
 		;
-
-	if (prev != -1)
-		close(prev);
 
 	return (0);
 }
